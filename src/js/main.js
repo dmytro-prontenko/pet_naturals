1941f75f1341bf6751a0bb2cecf68312e85578d8
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2024-02-23 10:00:43 +0100

    form fixes

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,19 @@ const menuLinks = document.querySelectorAll(".mobile-modal-link");
 const phone = document.querySelector(".hero_form-phone");
 const body = document.body;
 
+const todayNode = document.querySelector(".today-date")
+
+const today = new Date();
+
+const day = String(today.getDate()).padStart(2, '0');
+const month = String(today.getMonth() + 1).padStart(2, '0');
+const year = today.getFullYear();
+
+const formattedDate = `${day}.${month}.${year}`;
+
+todayNode.textContent=formattedDate
+
+
 menuLinks.forEach((menuLink) => {
   menuLink.addEventListener("click", toggleMenu);
 });
