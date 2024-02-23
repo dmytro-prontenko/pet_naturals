49dcc216cd8247863f6dedc10b5beee23f518c68
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2024-02-23 22:29:34 +0100

    final project cleanup

--- main.js
+++
@@ -1,4 +0,0 @@
-import './src/js/to-top.js';
-import './src/js/timer.js';
-import './src/js/modal.js';
-import './src/js/phone-mask.js';
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import './to-top.js';
+import './timer.js';
+import './modal.js';
+import './phone-mask.js';
